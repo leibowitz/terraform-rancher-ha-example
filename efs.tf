a57649d8f5833bde72e5058cfefeb979dc75f71c
Gianni Moschini <gianni.moschini+github@gmail.com>
2017-08-08 13:51:41 +0100

    Add EFS

---
+++ efs.tf
@@ -0,0 +1,29 @@
+#------------------------------------------#
+# AWS EFS Configuration
+#------------------------------------------#
+resource "aws_efs_file_system" "rancher_ha" {
+    creation_token = "rancher_ha_efs"
+}
+
+resource "aws_security_group" "rancher_ha_efs" {
+    name        = "${var.name_prefix}-efs-default"
+    description = "Rancher HA EFS Security Group"
+    vpc_id      = "${aws_vpc.rancher_ha.id}"
+}
+
+resource "aws_security_group_rule" "allow_efs" {
+    type              = "ingress"
+    from_port         = 2049
+    to_port           = 2049
+    protocol          = "tcp"
+    source_security_group_id = "${aws_security_group.rancher_ha_efs.id}"
+    security_group_id = "${aws_security_group.rancher_ha_efs.id}"
+}
+
+resource "aws_efs_mount_target" "rancher_ha" {
+  count = "${length(var.availability_zones)}"
+  file_system_id = "${aws_efs_file_system.rancher_ha.id}"
+  security_groups = ["${aws_security_group.rancher_ha_efs.id}"]
+  subnet_id  = "${element(aws_subnet.rancher_ha.*.id, count.index)}"
+}
+
--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,7 @@ output "elb_http_dns" {
 output "elb_https_dns" {
     value = "${aws_elb.rancher_ha_https.dns_name}"
 }
+
+output "efs_dns" {
+    value = ["${aws_efs_mount_target.rancher_ha.*.dns_name}"]
+}
